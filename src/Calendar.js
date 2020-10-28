e2aa5b0d2790c91e3aa53eae5abf8e067275806c
ccyjw8860 <ccyjw8860@gmail.com>
2020-10-28 20:29:56 +0900

    묶음 해제 기능 추가

--- src/Calendar.js
+++ src/Calendar.js
@@ -12,6 +12,22 @@ Array.prototype.shuffle = function () {
     return this;
 };
 
+function arrayIncludes(arr, testArr){
+    for(var i = 0; i<arr.length; i++){
+        let checker = []
+        for(var j = 0; j<arr[i].length; j++){
+            if(arr[i][j] === testArr[j]){
+                checker.push(true)
+            } else {
+                checker.push(false)
+            }
+        }
+        if (checker.every(check => check === true)){
+            return true
+        }
+    }
+    return false
+}
 
 const updateMonthIndexArray = (year, month, weekIndex) => {
     let monthIndexArray = [[1,1,1,1,1,1,1],
@@ -37,7 +53,7 @@ const updateMonthIndexArray = (year, month, weekIndex) => {
     return monthIndexArray;
 }
 
-const updateCalendar = (monthIndexArray,previousLastDay, weekindex) =>{
+const updateCalendar = (monthIndexArray, previousLastDay, weekindex) =>{
     let sDay = 1;
     let pFirstDay = previousLastDay - weekindex + 1;
     let nDay = 1;
@@ -64,6 +80,15 @@ const updateCalendar = (monthIndexArray,previousLastDay, weekindex) =>{
     return calendarArray
 }
 
+function array_equals(a, b){
+    return a.length === b.length && a.every((item,idx) => item === b[idx])
+}
+
+function diff(A, B) {
+    return A.filter(test => {
+      return B.findIndex(item => array_equals(item,test)) == -1;
+    });
+  }
 
 const popupWindow = () =>{
     const ul = document.createElement('ul');
@@ -78,15 +103,15 @@ const popupWindow = () =>{
     return ul
 }
 
-
 const Calendar = ({year, month, weekindex, previouslastday}) =>{
     const monthIndex = updateMonthIndexArray(year, month, weekindex);
     const calendar = updateCalendar(monthIndex, previouslastday, weekindex)
-    const [selected, setSelected] = useState([]);
+    const [daySelected, setDaySelected] = useState([]);
+    const [cSelected, setCSelected] = useState([]);
     const [cluster, setCluster] = useState([]);
+    const [colorIdx, setColorIdx] = useState(0);
     const colorClasses = ['bg_1', 'bg_2', 'bg_3', 'bg_4']
     const borderClasses = ['border_1', 'border_2', 'border_3', 'border_4']
-    const [colorIdx, setColorIdx] = useState(0);
 
     const fade_in = (e) =>{
         const element = e.target;
@@ -101,15 +126,17 @@ const Calendar = ({year, month, weekindex, previouslastday}) =>{
     const dayClustering = (e) =>{
         const root = document.getElementById('root');
         const ul = document.querySelector('.popupWindow');
-        setCluster([...cluster, selected]);
+        setCluster([...cluster, daySelected]);
 
-        selected.map((s, idx)=>{
+        daySelected.map((s, idx)=>{
             let target = document.getElementById(s);
+            target.classList.remove('selected');
+            target.classList.add('clustered');
             target.classList.add(colorClasses[colorIdx]);
             if(idx !== 0){
                 target.classList.add(borderClasses[colorIdx]);
                 let checkId = `_${s.split('_')[1]}_${Number(s.split('_')[2])-1}`
-                if(checkId !== selected[idx-1]){
+                if(checkId !== daySelected[idx-1]){
                     target.classList.add('border_black');
                 }
             }
@@ -117,13 +144,33 @@ const Calendar = ({year, month, weekindex, previouslastday}) =>{
                 target.classList.add('border_black');
             }
         })
-        setSelected([]);
+        setDaySelected([]);
         if(colorIdx !==3){
             setColorIdx(colorIdx + 1);
         }else{
             setColorIdx(0);
         }
         root.removeChild(ul);
+    }
+
+    const removeClustering = () =>{
+        const root = document.getElementById('root');
+        const ul = document.querySelector('.popupWindow');
+        root.removeChild(ul);
+        cSelected.map(c1=>{
+            c1.map(c2=>{
+                let element = document.getElementById(c2);
+                element.style.opacity = 1;
+                if(element.classList.value.includes('not')){
+                    element.className = 'not_current_month day';
+                }else{
+                    element.className = 'current_month day';
+                }
+            })
+        })
+        const res = diff(cluster, cSelected);
+        setCSelected([]);
+        setCluster(res);
     }
 
     const leftClick = (e) =>{
@@ -137,44 +184,77 @@ const Calendar = ({year, month, weekindex, previouslastday}) =>{
         if(classes.includes('selected')){
             e.target.classList.remove('selected');
             let result = [];
-            result = selected.filter(tomato=>{return(tomato!==e.target.id)});
-            setSelected(result);
+            result = daySelected.filter(tomato=>{return(tomato!==e.target.id)});
+            setDaySelected(result);
         }else{
-            e.target.classList.add('selected')
-            setSelected([...selected, e.target.id].sort());
+            if(!classes.includes('clustered')){
+                e.target.classList.add('selected');
+                setDaySelected([...daySelected, e.target.id].sort());
+            }else{
+                const id = e.target.id;
+                const basket = cluster.filter(c=>{
+                    return(c.includes(id));
+                }).flat();
+                if(arrayIncludes(cSelected, basket)){
+                    basket.map(b=>{
+                        let element = document.getElementById(b);
+                        element.style.opacity=1;
+                        element.classList.remove('cluster_s');                       
+                    })
+                    let res = diff(cSelected, [basket]);
+                    setCSelected(res);
+                }else{
+                    basket.map(b=>{
+                        let element = document.getElementById(b);
+                        element.style.opacity=0.5;
+                        element.classList.add('cluster_s');
+                    })
+                    setCSelected([...cSelected, basket]);
+                }
+            }
         }
     }
-    
+
     const rightClick = e =>{
+        e.preventDefault();
         console.log('오른쪽 클릭')
         const element = e.target;
         const topPosition = `${e.clientY}px`;
         const leftPosition = `${e.clientX}px`;
         const classes = element.classList.value;
         const root = document.querySelector('#root');
-        const lastElement = root.lastElement;
+        const lastElement = document.querySelector('.popupWindow')
         const ul = popupWindow();
-    
         if(root.lastElementChild === lastElement){
             root.removeChild(lastElement);
         }
-    
-        e.preventDefault();
+
         ul.style.top = topPosition;
         ul.style.left = leftPosition;
-    
+        
         if(classes.includes('selected')){
             root.appendChild(ul);
         }
         
-        const clusterBtn = document.getElementById('clustering_btn')
-        if(clusterBtn !== null){
-            clusterBtn.addEventListener('click', dayClustering);
+        if(element.classList.value.includes('clustered')){
+            if(element.classList.value.includes('cluster_s')){
+                root.appendChild(ul);
+                const clusterBtn = document.getElementById('clustering_btn');
+                if(clusterBtn !== null){
+                    clusterBtn.innerText = '묶음 해제';
+                    clusterBtn.addEventListener('click', removeClustering);
+                }
+            }
+        }else{
+            const clusterBtn = document.getElementById('clustering_btn')
+            if(clusterBtn !== null){
+                clusterBtn.addEventListener('click', dayClustering);
+            }
         }
     }
     
     const initStateAndHtml = () =>{
-        setSelected([]);
+        setDaySelected([]);
         setCluster([]);
         const days = document.querySelectorAll('.day');
         for(let i=0;i<days.length;i++){
@@ -196,18 +276,21 @@ const Calendar = ({year, month, weekindex, previouslastday}) =>{
                     let id = `_${w_idx}_${d_idx}`
                     if(monthIndex[w_idx][d_idx] === 1){
                         return(
-                        <div className='current_month day' id={id} onClick={leftClick} onContextMenu={rightClick} onMouseEnter={fade_in} onMouseLeave={fade_out}>{day}</div>
+                        <div className='current_month day' id={`${year}_${month}_${day}_|${id}`} onClick={leftClick} onContextMenu={rightClick} onMouseEnter={fade_in} onMouseLeave={fade_out}>{day}</div>
                             )
+                    }else if(monthIndex[w_idx][d_idx] === 0){
+                        return(<div className='not_current_month day' id={`${year}_${month-1}_${day}_|${id}`} onClick={leftClick} onContextMenu={rightClick} onMouseEnter={fade_in} onMouseLeave={fade_out}>{day}</div>)
                     }else{
-                        return(<div className='not_current_month day' id={id} onClick={leftClick} onContextMenu={rightClick} onMouseEnter={fade_in} onMouseLeave={fade_out}>{day}</div>)
+                        return(<div className='not_current_month day' id={`${year}_${month+1}_${day}_|${id}`} onClick={leftClick} onContextMenu={rightClick} onMouseEnter={fade_in} onMouseLeave={fade_out}>{day}</div>)
                     }
                 })}</div>
                 )
             })}
         </div>
     )
-
 }
+
+
 
 export default Calendar;
 
